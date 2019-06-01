ae1a14bc6866284df7c0c998921cd0364989bca8
AsenPlesnev <as.plesnev@gmail.com>
2019-06-02 00:02:19 +0300

    Implemented layout view.

--- SIS/SIS.WebServer/Controller.cs
+++ SIS/SIS.WebServer/Controller.cs
@@ -57,14 +57,21 @@ namespace SIS.MvcFramework
 
         protected ActionResult View([CallerMemberName] string view = null)
         {
-            string controllerName = GetType().Name.Replace("Controller", string.Empty);
+            //TODO: Support for layout
+            string controllerName = this.GetType().Name.Replace("Controller", string.Empty);
             string viewName = view;
 
             string viewContent = System.IO.File.ReadAllText("Views/" + controllerName + "/" + viewName + ".html");
 
             viewContent = ParseTemplate(viewContent);
 
-            HtmlResult htmlResult = new HtmlResult(viewContent);
+            string layoutContent = System.IO.File.ReadAllText("Views/_Layout.html");
+            layoutContent = ParseTemplate(layoutContent);
+            layoutContent = layoutContent.Replace("@RenderBody", viewContent);
+
+            viewContent = ParseTemplate(viewContent);
+
+            HtmlResult htmlResult = new HtmlResult(layoutContent);
 
             return htmlResult;
         }
