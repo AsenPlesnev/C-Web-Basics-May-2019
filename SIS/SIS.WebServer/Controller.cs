97f52b8ea459a41f7f1e491e0d1f19f1217d6a6e
AsenPlesnev <as.plesnev@gmail.com>
2019-06-02 13:58:15 +0300

    Implemented View Engine

--- SIS/SIS.WebServer/Controller.cs
+++ SIS/SIS.WebServer/Controller.cs
@@ -4,11 +4,14 @@ using SIS.HTTP.Requests;
 using SIS.MvcFramework.Extensions;
 using SIS.MvcFramework.Identity;
 using SIS.MvcFramework.Result;
+using SIS.MvcFramework.ViewEngine;
 
 namespace SIS.MvcFramework
 {
     public abstract class Controller
     {
+        private IViewEngine viewEngine = new SisViewEngine();
+
         protected Controller()
         {
             ViewData = new Dictionary<string, object>();
@@ -23,17 +26,6 @@ namespace SIS.MvcFramework
             : null;
 
         public IHttpRequest Request { get; set; }
-
-        private string ParseTemplate(string viewContent)
-        {
-            foreach (var param in ViewData)
-            {
-                viewContent = viewContent.Replace($"@Model.{param.Key}",
-                    param.Value.ToString());
-            }
-
-            return viewContent;
-        }
 
         protected bool IsLoggedIn()
         {
@@ -57,22 +49,24 @@ namespace SIS.MvcFramework
 
         protected ActionResult View([CallerMemberName] string view = null)
         {
-            //TODO: Support for layout
+            return this.View<object>(null, view);
+        }
+
+        protected ActionResult View<T>(T model = null, [CallerMemberName] string view = null)
+            where T : class
+        {
+            // TODO: Support for layout
             string controllerName = this.GetType().Name.Replace("Controller", string.Empty);
             string viewName = view;
 
             string viewContent = System.IO.File.ReadAllText("Views/" + controllerName + "/" + viewName + ".html");
-
-            viewContent = ParseTemplate(viewContent);
+            viewContent = this.viewEngine.GetHtml(viewContent, model);
 
             string layoutContent = System.IO.File.ReadAllText("Views/_Layout.html");
-            layoutContent = ParseTemplate(layoutContent);
-            layoutContent = layoutContent.Replace("@RenderBody", viewContent);
+            layoutContent = this.viewEngine.GetHtml(layoutContent, model);
+            layoutContent = layoutContent.Replace("@RenderBody()", viewContent);
 
-            viewContent = ParseTemplate(viewContent);
-
-            HtmlResult htmlResult = new HtmlResult(layoutContent);
-
+            var htmlResult = new HtmlResult(layoutContent);
             return htmlResult;
         }
 
