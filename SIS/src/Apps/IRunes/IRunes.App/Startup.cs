16df866cd4a433be7883550d438d4bfd1ec87ec3
AsenPlesnev <as.plesnev@gmail.com>
2019-06-08 14:47:26 +0300

    Deleted duplicate files and removed useless files

--- SIS/src/Apps/IRunes/IRunes.App/Startup.cs
+++ SIS/src/Apps/IRunes/IRunes.App/Startup.cs
@@ -1,11 +1,12 @@
 ﻿using IRunes.Data;
-
+using IRunes.Services;
 using SIS.MvcFramework;
 using SIS.MvcFramework.Routing;
+using SIS.MvcFramework.DependencyContainer;
 
 namespace IRunes.App
 {
-    public class Startup : IMvcApplication
+    public partial class Startup : IMvcApplication
     {
         public void Configure(IServerRoutingTable serverRoutingTable)
         {
@@ -15,8 +16,13 @@ namespace IRunes.App
             }
         }
 
-        public void ConfigureServices()
+        public void ConfigureServices(IServiceProvider serviceProvider)
         {
+            serviceProvider.Add<IAlbumService, AlbumService>();
+            serviceProvider.Add<IUserService, UserService>();
+            serviceProvider.Add<ITrackService, TrackService>();
+
+            //serviceProvider.Add
         }
     }
 }
